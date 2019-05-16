88f670f822cafde0e5d5bc5d47c322110348b619
Nick <nick.sanders@me.com>
2019-05-16 14:31:34 +0100

    Added initialisation file.

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+:Author:
+    Nicholas Sanders <N.Sanders@exeter.ac.uk>
+:Date:
+    16 May 2019
+"""
+from .__version__ import __version__
+from .pyvgk import vgk, vgkcon
---
+++ __version__.py
@@ -0,0 +1,7 @@
+"""
+:Author:
+    Nicholas Sanders <N.Sanders@exeter.ac.uk>
+:Date:
+    16 May 2019
+"""
+__version__ = "0.0.0"
